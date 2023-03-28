342203a46139e9c51fdcc54e00b347b7765bf202
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-03-28 10:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
+ s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
 
+  // Check if the reversed string is the same as the original
+  return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
